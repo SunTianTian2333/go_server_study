a2710c16f7081aecfe252172e45621617bc37dd0
unknown <2669516820@qq.com>
2025-03-07 23:43:48 +0800

    add user func: name change

--- user.go
+++ user.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net"
+	"strings"
 )
 
 type User struct {
@@ -60,8 +61,34 @@ func (this *User) Offline() {
 	this.server.BroadCast(this, "is offline now")
 }
 
+// send msg
+func (this *User) SendMessage(msg string) {
+	this.conn.Write([]byte(msg + "\n"))
+}
+
 // deal msg
 func (this *User) DoMessage(msg string) {
-	//receive msg from client
-	this.server.BroadCast(this, msg)
+	// //receive msg from client
+	// this.server.BroadCast(this, msg)
+	if msg == "who" {
+		this.server.maplock.Lock()
+		for _, user := range this.server.OnlineMap {
+			onlineMsg := "[" + user.addr + "]" + user.Name + ":" + "online \n"
+			this.SendMessage(onlineMsg)
+		}
+		this.server.maplock.Unlock()
+	} else if len(msg) > 7 && msg[:7] == "rename:" {
+		newName := strings.Split(msg, ":")[1]
+		_, ok := this.server.OnlineMap[newName]
+		if ok {
+			this.SendMessage("this name already exist")
+		} else {
+			this.server.maplock.Lock()
+			delete(this.server.OnlineMap, this.Name)
+			this.Name = newName
+			this.server.OnlineMap[newName] = this
+			this.SendMessage("name changed! your new name is : " + this.Name)
+			this.server.maplock.Unlock()
+		}
+	}
 }
